2862397de32d6180ae88ab8f332b175e73bfa32f
sifanahmed564-design <sifanahmed564@gmail.com>
2026-02-04 09:34:07 -0500

    Add files via upload
    i was add the Input listener in my script.js

--- gamelist.jon/script.js
+++ gamelist.jon/script.js
@@ -2,6 +2,16 @@ console.log("js console");
 
 let data;
 let grid = document.querySelector(".grid-container");
+var form = document.querySelector("form");
+
+
+  var newObj = {
+    title: titleInput.value,
+    publisher: pubInput.value,
+    releaseDate: dateInput.value
+  };
+
+
 
 var xhttp = new XMLHttpRequest();
 
@@ -34,6 +44,79 @@ xhttp.onreadystatechange = function(){
         });
     }
 };
+
+// LOAD DATA (localStorage first, otherwise XHR)
+if (localStorage.getItem("datalist")) {
+  data = JSON.parse(localStorage.getItem("datalist"));
+  console.log("Loaded from localStorage");
+  if (grid) {
+    makeCards();
+  }
+} else {
+  var xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      data = JSON.parse(this.responseText);
+      console.log("Loaded from gameData.json");
+
+      localStorage.setItem("datalist", JSON.stringify(data));
+      console.log("Saved starter data to localStorage");
+
+      if (grid) {
+        makeCards();
+      }
+    }
+  };
+
+  xhttp.open("GET", "gameData.json", true);
+  xhttp.send();
+}
+
+
+// RENDER CARDS
+function makeCards() {
+  grid.innerHTML = "";
+
+  data.forEach(function (game) {
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let textData =
+      "<div class='game-title'>" + game.title + "</div>" +
+      "<div>Publisher: " + game.publisher + "</div>" +
+      "<div>Release Date: " + game.releaseDate + "</div>";
+
+    card.innerHTML = textData;
+    grid.appendChild(card);
+  });
+
+  console.log("cards refreshed");
+}
+
+
+
+
+form.addEventListener("submit",function (e){
+    e.preventDefault();
+let title = titleInput.value;
+let publisher = devInput.value;
+let releaseDate = releaseDateInput.value;
+let gifSrc = gifInput.value;
+let imgSrc = imgInput.value;
+let newObj = {
+    "id": getNextId(),
+    "title": title,
+    "releaseDate":publisher,
+    "imgSrc":releaseDate, 
+    "gifSrc":gifSrc}
+    submitData(newObj);
+    form.reset();
+
+})
+
+
+
 
 const submitButton = document.getElementById("submit");
 
